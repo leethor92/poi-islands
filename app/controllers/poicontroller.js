3f83b8953e1ef8dc37bf700e123751c9946f3ef1
DESKTOP-TMDJO97\Lee <leethornton1992@hotmail.com>
2019-03-05 21:36:50 +0000

    views functional after handlebars plugin install

--- app/controllers/poicontroller.js
+++ app/controllers/poicontroller.js
@@ -1,17 +1,17 @@
 const pointsOfInterest = {
   index: {
     handler: function(request, h) {
-      return h.file('./app/views/main.hbs');
+      return h.view('main',{title: 'Welcome to the Irish Islands POI'});
     }
   },
   signup: {
     handler: function(request, h) {
-      return h.file('./app/views/signup.hbs')
+      return h.view('signup',{title: 'Sign up for a POI account'})
     }
   },
   login: {
     handler: function(request, h) {
-      return h.file('./app/views/login.hbs')
+      return h.view('login',{title: 'Login to your POI account'})
     }
    }
 }
