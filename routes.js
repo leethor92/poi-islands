bd02084a0900fe113456e841da568a5fe6fb1a68
DESKTOP-TMDJO97\Lee <leethornton1992@hotmail.com>
2019-03-05 21:11:32 +0000

    signup & login routes added with navigation enable, not fully functional yet!

--- routes.js
+++ routes.js
@@ -2,6 +2,8 @@ const pointsOfInterest = require('./app/controllers/poicontroller');
 
 module.exports = [
   { method: 'GET', path: '/', config: pointsOfInterest.index },
+  { method: 'GET', path: '/signup', config: pointsOfInterest.signup },
+  { method: 'GET', path: '/login', config: pointsOfInterest.login },
   {
     method: 'GET', path: '/{param*}',
     handler: {
